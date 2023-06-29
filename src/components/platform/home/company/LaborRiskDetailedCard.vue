3e7efe4a79d72a72fb0706994333d863fc4f19a6
Lucas Corrêa <lucascorrea@MacBook-Air-de-Lucas.local>
2023-06-29 08:09:50 -0300

    WIP

--- src/components/platform/home/company/LaborRiskDetailedCard.vue
+++ src/components/platform/home/company/LaborRiskDetailedCard.vue
@@ -2,24 +2,53 @@
   <q-card class="home-company-labor-risk-detailed-card column q-pb-md q-px-md">
     <div class="home-company-labor-risk-detailed-card-header column">
       <div class="home-company-labor-risk-detailed-card-info-container">
-        <q-card-section class="home-company-labor-risk-detailed-card-title">
+        <q-card-section
+          class="home-company-labor-risk-detailed-card-title"
+          style="height: 65px"
+        >
           <h4>Pesquisa de Desligamento</h4>
           <h6>Notas de 1 a 10</h6>
         </q-card-section>
       </div>
-      <!-- <Column v-if="showChart" :data="laborRiskDataChartConverted" :height="450" /> -->
+      <Column v-if="false" :data="laborRiskDataChartConverted" :height="450" />
 
-      <div class=""></div>
+      <div class="q-mt-lg card">
+        <div
+          class="chart-card"
+          v-for="(l, index) in laborRiskDataChartConverted.slice(1)"
+          :key="l"
+        >
+          <div class="chart__row-title">
+            <div
+              class="chart__row-label"
+              :style="`min-width: 80px; width: ${l[1] * 10}%; `"
+            >
+              <p>Pergunta {{ index + 1 }}</p>
+              <p v-if="l[1] > 4">{{ l[1] }}</p>
+            </div>
+          </div>
+          <div class="chart__row" :id="formatId(l[0])">
+            <div
+              class="chart__row__value"
+              :style="`width: ${
+                l[1] * 10
+              }%; background: linear-gradient(90deg, #1a27b7 0%, ${
+                l[1] < 5 ? '#34a67c' : '#ff4690'
+              } 100%);`"
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   </q-card>
 </template>
 
 <script>
-//import Column from "./../../../general/charts/Column.vue";
+import Column from "./../../../general/charts/Column.vue";
 export default {
-  /*   components: {
+  components: {
     Column,
-  }, */
+  },
   props: ["laborRisks"],
   data() {
     return {
@@ -32,6 +61,19 @@ export default {
     this.showChart = true;
   },
   methods: {
+    formatId(name) {
+      const format = (str) => {
+        return str
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .replace(/([^\w]+|\s+)/g, "-")
+          .replace(/\-\-+/g, "-")
+          .replace(/(^-+|-+$)/, "");
+      };
+
+      return "chart__row-" + format(name);
+    },
+
     converDataChart: function () {
       this.laborRiskDataChartConverted.push([
         "Risco Trabalhista",
@@ -75,5 +117,52 @@ export default {
     width: 90vw;
     height: 20vh;
   }
+}
+
+.chart-card {
+  height: auto;
+}
+
+.chart__row-title {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.chart__row {
+  height: 15px;
+  width: 80%;
+  border-radius: 10px;
+  background-color: #e1e1e1;
+  margin: 0 auto;
+}
+
+.chart__row__value {
+  height: 15px;
+  border-radius: 10px;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+}
+
+.chart__row-label p {
+  margin: 0px;
+  margin-top: 15px;
+}
+
+.chart__row-label {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  color: #b1b1b1;
+}
+
+h4,
+h6 {
+  color: #b1b1b1;
+  text-align: left;
+  margin: 0;
+  margin-left: 10%;
 }
 </style>
